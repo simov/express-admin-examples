fc53b34c29f5755e668343fad3956d339bdf0261
simov <simeonvelichkov@gmail.com>
2013-06-02 18:36:00 +0300

    simple-schema fix

--- fixtures/simple/schema.sql
+++ fixtures/simple/schema.sql
@@ -2,6 +2,9 @@ SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
+DROP SCHEMA IF EXISTS `express-admin-simple` ;
+CREATE SCHEMA IF NOT EXISTS `express-admin-simple` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ;
+USE `express-admin-simple` ;
 
 -- -----------------------------------------------------
 -- Table `item`
@@ -196,6 +199,7 @@ CREATE  TABLE IF NOT EXISTS `recipe_ref` (
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
+USE `express-admin-simple` ;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
