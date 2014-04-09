f93af306b35cf9c77014dbf80b89dfed8117bfb4
simov <simeonvelichkov@gmail.com>
2014-04-09 16:55:58 +0300

    Fixed display of base64 images inside the listview

--- examples/custom/static/binary.js
+++ examples/custom/static/binary.js
@@ -2,12 +2,12 @@
 $(function () {
     if (!$('.x-filter').length) return;
     var index = $('.ex-table th').index($('.ex-table th:contains(binary)'));
-    if (!index) return;
-
-    $('.ex-table tr:gt(0)').each(function (idx) {
-        var blob = $('td', this).eq(index),
-            base64 = blob.text();
-            
-        blob.html('<img src="'+base64+'" />');
-    });
+    if (index != -1) {
+        $('.ex-table tr:gt(0)').each(function (idx) {
+            var blob = $('td', this).eq(index),
+                base64 = blob.text();
+                
+            blob.html('<img src="'+base64+'" />');
+        });
+    }
 });
