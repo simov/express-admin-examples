a98f6107215c2ce616a45f5ac5ea2692874f6040
simov <simeonvelichkov@gmail.com>
2014-06-09 11:15:00 +0300

    Fix path script to support all engines

--- path.js
+++ path.js
@@ -2,9 +2,12 @@
 
 var fs = require('fs'),
     path = require('path');
-var fpath = path.join(__dirname, './config/mysql/custom.json');
 
-var data = fs.readFileSync(fpath, 'utf8');
-fs.writeFileSync(fpath,
-    data.replace(/\/home\/mighty\/node\/modules\/express-admin-examples/g, __dirname)
-);
+['mysql', 'pg', 'sqlite'].forEach(function (engine) {
+    var fpath = path.join(__dirname, './config/', engine, '/custom.json');
+
+    var data = fs.readFileSync(fpath, 'utf8');
+    fs.writeFileSync(fpath,
+        data.replace(/\/home\/mighty\/node\/modules\/express-admin-examples/g, __dirname)
+    );
+});
