7e1dcd57d6f62c68d7d0b046c99cd3e931b898c6
simov <simeonvelichkov@gmail.com>
2014-07-02 19:04:46 +0300

    Improve sqlite import script

--- fixtures/sqlite/import.js
+++ fixtures/sqlite/import.js
@@ -1,13 +1,15 @@
 
-var fs = require('fs');
-if (fs.existsSync('db.sqlite'))
-    fs.unlinkSync('db.sqlite');
+var fs = require('fs'),
+    path = require('path');
+
+if (fs.existsSync(path.join(__dirname,'db.sqlite')))
+    fs.unlinkSync(path.join(__dirname,'db.sqlite'));
 
 var sqlite3 = require('sqlite3').verbose();
-var db = new sqlite3.Database('db.sqlite');
+var db = new sqlite3.Database(path.join(__dirname,'db.sqlite'));
 
-var schema = fs.readFileSync('schema.sql', 'utf8');
-var insert = fs.readFileSync('insert.sql', 'utf8');
+var schema = fs.readFileSync(path.join(__dirname,'schema.sql'), 'utf8');
+var insert = fs.readFileSync(path.join(__dirname,'insert.sql'), 'utf8');
 
 db.serialize(function() {
     db.exec(schema);
