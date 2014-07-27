f86953f16ad81d099b5594b9ad5720d762cf6b75
simov <simeonvelichkov@gmail.com>
2014-07-27 18:47:25 +0300

    Add upload control for image column in item table

--- config/custom/static/images.js
+++ config/custom/static/images.js
@@ -1,17 +1,35 @@
 
 $(function () {
-    // show all imgur urls as images inside the Listview
-    $('.x-table td:contains(imgur)').each(function (index) {
-        $(this).html('<img src="'+$(this).text().trim()+'" style="width:100px" />')
+    // show all images inside the ListView
+    $('.x-table tbody tr').each(function (index) {
+        // the image column is second
+        var td =  $('td:eq(1)', this);
+        // get the image url
+        var url = td.text().trim();
+
+        // skip on missing url
+        if (!url) return;
+        // by default all uploads go to express-admin/public/upload
+        if (!url.match(/^http/)) url = '/upload/' + url;
+
+        // add image to this table cell
+        td.html('<img src="'+url+'" style="width:100px" />');
     });
 
-    // show all imgur urls as images inside the Editview
-    $('.x-table [type=text]').each(function (index) {
-        if ($(this).val().match(/.*imgur.*/)) {
-            $(this).parent().append('<img src="'+$(this).val()+'" style="margin-top:10px" />')
-        }
+    // show all images inside the EditView
+    $('.x-table [type=file]').each(function (index) {
+        var holder = $(this).parent().prev();
+        // get the image url
+        var url = holder.find('[type=text]').val();
+
+        // skip on missing url
+        if (!url) return;
+        // by default all uploads go to express-admin/public/upload
+        if (!url.match(/^http/)) url = '/upload/' + url;
+
+        // add image to this table cell
+        holder.append('<img src="'+url+'" style="margin-top:10px" />');
     });
 
-    // Hint: you can add only a link to the image and hook a jQuery plugin to it
-    // that opens up the image in a modal popup with faded background for example
+    // Hint: you can use some jQuery plugin here
 });
