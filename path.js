97e9cf8a8fa76dc20f6ae3e5d01d3f86adb2e61e
John Hall <john@real-currents.com>
2015-06-10 10:00:49 -0700

    another fix for windows paths

--- path.js
+++ path.js
@@ -5,19 +5,17 @@ var fs = require('fs'),
     path = require('path'),
     util = require('util');
 
-['mysql', 'pg', 'sqlite'].forEach(function (engine) {
-	console.error( util.inspect(process.env) +"\n" );
-	
+['mysql', 'pg', 'sqlite'].forEach(function (engine) {	
 	var cwd = (process.env.OS.match(/Windows/) !== null)?
 			__dirname.replace(/\\/g, "/"):
 			__dirname;
-	
-	console.error( "CWD: "+ cwd +"\n" );
 	
     var fpath = path.join(cwd, './config/', engine, '/custom.json');
 
     var data = fs.readFileSync(fpath, 'utf8');
     fs.writeFileSync(fpath,
-        data.replace(/\/home\/mighty\/node\/modules\/express-admin-examples/g, cwd)
+        data.replace(/\/home\/mighty\/node\/modules\/express-admin-examples/g, function(m){
+			return cwd;
+		})
     );
 });
